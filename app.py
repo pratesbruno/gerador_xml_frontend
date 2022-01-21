70ebc89014a70e49cf123a48817afa77527132e9
pratesbruno <bruno.sp.prates@gmail.com>
2022-01-21 12:14:43 -0300

    api funcionando localmente

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from PIL import Image
+from io import StringIO
+import requests
 
 
 st.set_page_config(layout="wide")
@@ -25,9 +27,15 @@ col1.markdown(
 ### Faça upload do arquivo .csv
 ''')
 
-uploaded_file = col1.file_uploader("Escolha um arquivo", type=['csv'])
-
-
+arquivo_carregado = col1.file_uploader("Escolha um arquivo", type=['csv'])
+try:
+     arquivo_lido = arquivo_carregado.read()
+     s=str(arquivo_lido,'utf-8')
+     data = StringIO(s)
+     col1.write(type(data))
+     
+except:
+     col1.write('Nenhum arquivo carregado até o momento.')
 
 col1.markdown(
 '''
@@ -50,18 +58,39 @@ tipo_produto_escolhido = col1.selectbox(
      'Escolha o tipo de produto',
      ('exames','vacinas'))
 
+nome_do_arquivo=f'{operadora_escolhida}_{tipo_produto_escolhido}.csv'
+
 col1.markdown(
 '''
 ### Clique no botão abaixo para gerar o(s) XML(s)
 ''')
 
 
+def gerar_xml():
+     
+     api_endpoint = 'http://localhost:8000/gerar_xml'
+     
 
-col1.button('Gerar XML')
+     payload={'tipo_input': 'csv',
+     'operadora': operadora_escolhida,
+     'seq_transacao': '20220119199',
+     'tipo_produto': tipo_produto_escolhido}
 
+     files=[
+     ('file',('file_name',arquivo_carregado.getbuffer(),'text/csv'))
+     ]
+
+     headers = {}
+
+     response = requests.request("POST", api_endpoint, headers=headers, data=payload, files=files)
+
+     col1.write(response.text)
+
+col1.button('Gerar XML', on_click=gerar_xml)
 
 
 # Different ways to use the API
 
-if uploaded_file:
-    st.download_button('Download CSV', uploaded_file, )
+if arquivo_carregado:
+    st.download_button(label='Download CSV', data=s, mime='text/csv',
+                       file_name=f'{nome_do_arquivo}')
