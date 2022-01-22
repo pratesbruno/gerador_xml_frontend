2acff893ce6fd82600321477870d9e09ed0e8dcb
pratesbruno <bruno.sp.prates@gmail.com>
2022-01-21 21:26:32 -0300

    double backslashes

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 from PIL import Image
 from io import StringIO
 import requests
-
+import json
 
 st.set_page_config(layout="wide")
 col1, col2, col3 = st.columns((3,1,1))
+conteudo_xml= None
 
 image = Image.open('beep_logo.png')
 col3.image(image, caption='Beep Saúde')
@@ -32,7 +33,6 @@ try:
      arquivo_lido = arquivo_carregado.read()
      s=str(arquivo_lido,'utf-8')
      data = StringIO(s)
-     col1.write(type(data))
      
 except:
      col1.write('Nenhum arquivo carregado até o momento.')
@@ -58,7 +58,7 @@ tipo_produto_escolhido = col1.selectbox(
      'Escolha o tipo de produto',
      ('exames','vacinas'))
 
-nome_do_arquivo=f'{operadora_escolhida}_{tipo_produto_escolhido}.csv'
+nome_do_arquivo=f'{operadora_escolhida}_{tipo_produto_escolhido}.xml'
 
 col1.markdown(
 '''
@@ -68,29 +68,35 @@ col1.markdown(
 
 def gerar_xml():
      
-     api_endpoint = 'http://localhost:8000/gerar_xml'
-     
+     if arquivo_carregado:
+          api_endpoint = 'http://localhost:8000/gerar_xml'
 
-     payload={'tipo_input': 'csv',
-     'operadora': operadora_escolhida,
-     'seq_transacao': '20220119199',
-     'tipo_produto': tipo_produto_escolhido}
+          payload={'tipo_input': 'csv',
+          'operadora': operadora_escolhida,
+          'seq_transacao': '20220119199',
+          'tipo_produto': tipo_produto_escolhido}
 
-     files=[
-     ('file',('file_name',arquivo_carregado.getbuffer(),'text/csv'))
-     ]
+          files=[
+          ('file',('file_name',arquivo_carregado.getbuffer(),'text/csv'))
+          ]
 
-     headers = {}
+          headers = {}
 
-     response = requests.request("POST", api_endpoint, headers=headers, data=payload, files=files)
+          resposta = requests.request("POST", api_endpoint, headers=headers, data=payload, files=files)
 
-     col1.write(response.text)
+          dict_resposta = json.loads((resposta.text))
+
+          col1.write(dict_resposta['mensagem'])
+
+          conteudo_xml = dict_resposta['conteudo_xml']
+
+          conteudo_namespace = dict_resposta['xml_com_namespaces']
+
+          col1.download_button(label='Download XML', data=conteudo_namespace, mime='xml',
+                         file_name=f'{nome_do_arquivo}')
+
+     else:
+          col1.write("Nenhum arquivo csv escolhido. Por favor, escolha um arquivo primeiro.")
 
 col1.button('Gerar XML', on_click=gerar_xml)
 
-
-# Different ways to use the API
-
-if arquivo_carregado:
-    st.download_button(label='Download CSV', data=s, mime='text/csv',
-                       file_name=f'{nome_do_arquivo}')
