367bbf3e258b5456829bb1a09d2c4f30ec65ae2c
pratesbruno <bruno.sp.prates@gmail.com>
2022-01-25 18:43:54 -0300

    incluindo mensagem de erro

--- app.py
+++ app.py
@@ -10,24 +10,23 @@ import zipfile
 st.set_page_config(layout="wide")
 col1, col2, col3 = st.columns((3,1,1))
 
+# Define os diferentes modulos do site
+modulos = ["Gerar XMLs", "Gerar guias PDF", "Buscar senha Bradesco", "Baixar infos metabase"]
 
-# Inclúi sidebar
-seletor_modulo = st.sidebar.selectbox(
-    "O que você deseja fazer?",
-    ("Gerar XMLs", "Gerar guias PDF", "Consultar infos")
-)
+# Inclui sidebar
+seletor_modulo = st.sidebar.selectbox("O que você deseja fazer?", (modulos[0], modulos[1], modulos[2], modulos[3]))
+
 
 # Inclúi logo da Beep no canto superior direito
 image = Image.open('beep_logo.png')
 col3.image(image, caption='Beep Saúde')
 
-if seletor_modulo == "Gerar XMLs":
+if seletor_modulo == modulos[0]:
      col1.markdown('# Gerador XML versão TISS')
 
      col1.markdown(
      '''
      ## Instruções
-
      Faça o upload do arquivo .csv, preencha com as informações necessárias (operadora e tipo de produto) e clique no botão para gerar o XML.
      ''')
 
@@ -86,7 +85,6 @@ if seletor_modulo == "Gerar XMLs":
           if arquivo_carregado:
 
                # Dados para chamar a API
-               api_endpoint_local = 'http://localhost:8000/gerar_xml'
                api_endpoint = 'https://gerador-xml-api-6doupywhkq-ew.a.run.app/gerar_xml'
 
                payload={'tipo_input': 'csv',
@@ -102,58 +100,65 @@ if seletor_modulo == "Gerar XMLs":
                dict_resposta = json.loads((resposta.text))
 
                # Captura informações geradas pela API
-               mensagem = dict_resposta['mensagem']
-               arquivos_xml = dict_resposta['arquivos_xml']
-               num_guias = dict_resposta['num_guias']
-               numero_arquivos_xml = dict_resposta['numero_arquivos_xml']
-               valor_total = dict_resposta['valor_total_arquivos']
-               lista_sequencial_transacao = dict_resposta['lista_sequencial_transacao']
+               mensagem = dict_resposta.get('mensagem')
+               arquivos_xml = dict_resposta.get('arquivos_xml')
+               num_guias = dict_resposta.get('num_guias')
+               numero_arquivos_xml = dict_resposta.get('numero_arquivos_xml')
+               valor_total = dict_resposta.get('valor_total_arquivos')
+               lista_sequencial_transacao = dict_resposta.get('lista_sequencial_transacao')
+               erro = dict_resposta.get('error')
 
                # Exibe mensagem de sucesso/erro da API
                col1.write(mensagem)
-
-               # Exibe infos da geração do XML
-               if numero_arquivos_xml == 1:
-                    infos = "Foi gerado 1 arquivo xml, referente a "
-               else:
-                    infos = f"Foram gerados {numero_arquivos_xml} arquivos xml, referentes a "
                
-               valor_total_formato_real = '{:,.2f}'.format(float(valor_total)).replace('.','aux').replace(',','.').replace('aux',',')
-               infos = infos + f"{num_guias} guias distintas e um total de R$ {valor_total_formato_real}."
-               
-               col1.write(infos)
-               #xml_com_namespaces = dict_resposta['xml_com_namespaces']
-
-               # Cria um botão de download para baixar o(s) arquivo(s) XML
-               # Caso tenha mais de 1 arquivo XML, gera um ZIP. Caso contrário, gera apenas um arquivo XML
-               if len(arquivos_xml) > 1:
-                    zip_buffer = BytesIO()
-                    with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
-                         for i in range(len(arquivos_xml)):
-                              nome_arquivo = f'{operadora_escolhida}_{data_registro_transacao}_{tipo_produto}_{i+1}.xml'
-                              conteudo_arquivo = arquivos_xml[i]
-                              zip_file.writestr(nome_arquivo, conteudo_arquivo)
+               # Checa se a API funcionou corretamente (verificando se ela retornou arquivos XML). Caso positivo, cria o botao para baixar os XML.
+               if arquivos_xml:
+                    # Exibe infos da geração do XML
+                    if numero_arquivos_xml == 1:
+                         infos = "Foi gerado 1 arquivo xml, referente a "
+                    else:
+                         infos = f"Foram gerados {numero_arquivos_xml} arquivos xml, referentes a "
                     
-                    col1.download_button(label='Download XML (zip)', data=zip_buffer.getvalue(), mime='application/zip',
-                                   file_name=f'{operadora_escolhida}_{data_registro_transacao}_{tipo_produto}_zip.zip')
+                    valor_total_formato_real = '{:,.2f}'.format(float(valor_total)).replace('.','aux').replace(',','.').replace('aux',',')
+                    infos = infos + f"{num_guias} guias distintas e um total de R$ {valor_total_formato_real}."
+                    
+                    col1.write(infos)
 
+                    # Cria um botão de download para baixar o(s) arquivo(s) XML
+                    # Caso tenha mais de 1 arquivo XML, gera um ZIP. Caso contrário, gera apenas um arquivo XML
+                    if len(arquivos_xml) > 1:
+                         zip_buffer = BytesIO()
+                         with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
+                              for i in range(len(arquivos_xml)):
+                                   nome_arquivo = f'{operadora_escolhida}_{data_registro_transacao}_{tipo_produto}_{i+1}.xml'
+                                   conteudo_arquivo = arquivos_xml[i]
+                                   zip_file.writestr(nome_arquivo, conteudo_arquivo)
+                         
+                         col1.download_button(label='Download XML (zip)', data=zip_buffer.getvalue(), mime='application/zip',
+                                        file_name=f'{operadora_escolhida}_{data_registro_transacao}_{tipo_produto}_zip.zip')
+
+                    else:
+                         col1.download_button(label='Download XML', data=arquivos_xml[0], mime='xml',
+                                        file_name=f'{operadora_escolhida}_{data_registro_transacao}_{tipo_produto}_1.xml')
+
+               # Caso a api nao tenha retornado arquivos XML, é porque ela falhou. Nesse caso, mostra a mensagem de erro.
                else:
-                    col1.download_button(label='Download XML', data=arquivos_xml[0], mime='xml',
-                                   file_name=f'{operadora_escolhida}_{data_registro_transacao}_{tipo_produto}_1.xml')
+                    col1.write(f'Mensagem de erro: {erro}')
 
           # Caso não tenha nenhum arquivo csv carregado, exibe uma mensagem de erro.
           else:
                col1.write("Nenhum arquivo csv escolhido. Por favor, escolha um arquivo primeiro.")
 
-
      col1.button('Gerar XML', on_click=gerar_xml)
 
-elif seletor_modulo == "Gerar guias PDF":
+elif seletor_modulo == modulos[1]:
      col1.markdown('# Gerador de guias formato PDF')
-
      col1.write("Funcionalidade ainda não implementada")
 
-elif seletor_modulo == "Consultar infos":
-     col1.markdown('# Consultar informações de faturamento')
+elif seletor_modulo == modulos[2]:
+     col1.markdown('# Buscar senha Bradesco')
+     col1.write("Funcionalidade ainda não implementada")     
 
+elif seletor_modulo == modulos[3]:
+     col1.markdown('# Consultar informações de faturamento')
      col1.write("Funcionalidade ainda não implementada")
